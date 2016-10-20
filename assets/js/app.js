88e5842e4362d790d991edc2b64aae79cabe35ad
tmontdev <the.monteiro.dev@gmail.com>
2016-10-20 05:07:01 -0300

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,18 @@ $(function(){
 	 var headerHeight = $('.header').height();
 	$('.home').css('padding-top', headerHeight+'px');
 });
+$(function(){
+	$('.who-button').click(function(event) {
+		$('.mission-button').removeClass('actived');
+		$(this).addClass('actived');
+		$('.mission').removeClass('actived');
+		$('.who').addClass('actived').addClass('animated').addClass('fadeIn');
+	});
+	$('.mission-button').click(function(event) {
+		$('.who-button').removeClass('actived');
+		$(this).addClass('actived');
+		$('.who').removeClass('actived').removeClass('animated').removeClass('fadeIn');
+		$('.mission').addClass('actived').addClass('animated').addClass('fadeIn');
+	});
+
+});
