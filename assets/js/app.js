b141d05a43c7b4c2cb8976c864280cde01d69051
tmontdev <the.monteiro.dev@gmail.com>
2016-10-26 02:37:39 -0300

    zika

--- assets/js/app.js
+++ assets/js/app.js
@@ -248,18 +248,3 @@ $(function(){
 
 	contact_social.css('padding-bottom', (contact_diff+60)+'px');
 })();
-
-
-
-//progress bar ... estudar como funciona
-var steps = $(".step");
-console.dir(steps);
-
-setTimeout(function() {
-  steps.each(function(index) {
-    var _t = $(this);
-    setTimeout(function() {
-      _t.addClass('done');
-    }, 1250*index*1.5);
-  });
-}, 500);
