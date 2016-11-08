c36ed58f00e1947db0477fe0abd1ab639962045c
Marcos Felipe <omarkdev@gmail.com>
2016-11-08 04:36:00 -0200

    Removed code unseless

--- assets/js/app.js
+++ assets/js/app.js
@@ -168,21 +168,6 @@ $(function(){
 	contact_social.css('padding-bottom', (contact_diff+60)+'px');
 })();
 
-
-
-//progress bar ... estudar como funciona
-var steps = $(".step");
-console.dir(steps);
-
-setTimeout(function() {
-  steps.each(function(index) {
-    var _t = $(this);
-    setTimeout(function() {
-      _t.addClass('done');
-    }, 1250*index*1.5);
-  });
-}, 500);
-
 $(function(){
 	$('.switch-on').click(function(event) {
 		$('#header').addClass('header-actived').addClass('animated').addClass('fadeInLeft');
